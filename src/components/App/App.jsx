558c055a44746472f86f3fdac9676991a629495f
Svetlana Trotsenko <lanalemishko@gmail.com>
2022-09-09 00:18:13 +0300

    refactoring-code

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,61 +3,40 @@ import { GlobalStyle } from './App.styled';
 import { ContactForm } from '../ContactForm/ContactForm';
 import { Filter } from '../Filter/Filter';
 import { ContactList } from '../ContactList/ContactList';
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-export class App extends Component {
-  state = {
-    contacts: [
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(window.localStorage.getItem('contacts')) ?? [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+    ]
+  );
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const contactFromLS = JSON.parse(localStorage.getItem('contacts'));
-    if (contactFromLS) {
-      this.setState({
-        contacts: [...contactFromLS],
-      });
-    }
-  }
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  componentDidUpdate(prevProps, prevState) {
-    const { contacts } = this.state;
-
-    if (prevState.contacts !== contacts) {
-      return localStorage.setItem('contacts', JSON.stringify(contacts));
-    }
-  }
-
-  onSubmitForm = ({ name, number }, action) => {
-    const doubleContact = this.state.contacts.find(
-      contact => contact.name === name
-    );
+  const onSubmitForm = ({ name, number }, action) => {
+    const doubleContact = contacts.find(contact => contact.name === name);
 
     doubleContact
       ? Notify.warning(`${name} is already in contact`)
-      : this.setState(prevState => {
-          return {
-            contacts: [
-              ...prevState.contacts,
-              { id: nanoid(), name: name, number: number },
-            ],
-          };
-        });
+      : setContacts(prevState => [
+          ...prevState,
+          { id: nanoid(), name: name, number: number },
+        ]);
 
     action.resetForm();
   };
 
-  onFilterContacts = () => {
-    const { filter } = this.state;
-
-    const filteredContacts = this.state.contacts.filter(
+  const onFilterContacts = () => {
+    const filteredContacts = contacts.filter(
       ({ name, number }) =>
         name.toLowerCase().includes(filter.toLowerCase()) ||
         number.toLowerCase().includes(filter.toLowerCase())
@@ -66,47 +45,38 @@ export class App extends Component {
     return filteredContacts;
   };
 
-  onFilterInput = event => {
-    return this.setState({ filter: event.target.value });
+  const onFilterInput = event => {
+    return setFilter(event.target.value);
   };
 
-  onDeleteContact = id => {
-    return this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(contact => {
-          return contact.id !== id;
-        }),
-      };
-    });
-  };
-
-  render() {
-    const { onSubmitForm, onFilterInput, onFilterContacts, onDeleteContact } =
-      this;
-
-    return (
-      <Box
-        p={4}
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        flexDirection="column"
-      >
-        <GlobalStyle />
-        <Box as="h2" mb={3}>
-          Phonebook
-        </Box>
-        <ContactForm onSubmitForm={onSubmitForm} />
-
-        <Box as="h2" mb={3}>
-          Contacts
-        </Box>
-        <Filter onFilterInput={onFilterInput} />
-        <ContactList
-          contacts={onFilterContacts()}
-          onDeleteContact={onDeleteContact}
-        />
-      </Box>
+  const onDeleteContact = id => {
+    return setContacts(prevState =>
+      prevState.filter(contact => contact.id !== id)
     );
-  }
-}
+  };
+
+  return (
+    <Box
+      p={4}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+    >
+      <GlobalStyle />
+      <Box as="h2" mb={3}>
+        Phonebook
+      </Box>
+      <ContactForm onSubmitForm={onSubmitForm} />
+
+      <Box as="h2" mb={3}>
+        Contacts
+      </Box>
+      <Filter onFilterInput={onFilterInput} />
+      <ContactList
+        contacts={onFilterContacts()}
+        onDeleteContact={onDeleteContact}
+      />
+    </Box>
+  );
+};
